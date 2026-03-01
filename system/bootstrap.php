7837a5eb9da71c061826dbacf984ebe25d9c81fe
suchanon456 <137870206+suchanon456@users.noreply.github.com>
2026-03-01 14:02:28 +0700

    Add files via upload

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -1,4 +1,25 @@
 <?php
+// Define base constants
+if (!defined('BASEPATH')) {
+    define('BASEPATH', __DIR__ . DIRECTORY_SEPARATOR);
+}
+
+if (!defined('APPPATH')) {
+    define('APPPATH', __DIR__ . '/../app' . DIRECTORY_SEPARATOR);
+}
+
+if (!defined('WRITEPATH')) {
+    define('WRITEPATH', __DIR__ . '/../writable' . DIRECTORY_SEPARATOR);
+}
+
+if (!defined('APP_NAMESPACE')) {
+    define('APP_NAMESPACE', 'App');
+}
+
+if (!defined('ENVIRONMENT')) {
+    define('ENVIRONMENT', $_ENV['CI_ENVIRONMENT'] ?? 'development');
+}
+
 // โหลด autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -7,7 +28,8 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 // โหลด config
-$config = require __DIR__ . '/../app/Config/App.php';
+require __DIR__ . '/../app/Config/App.php';
+$config = new Config\App();
 
 // เริ่มต้นระบบด้วยศีล 5
 $precepts = new Silpa5PHP\Core\FivePrecepts\PreceptEngine($config);
